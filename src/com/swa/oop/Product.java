1b8f24abfddd9ed3f1164dde2a3c2979666a2c29
NATTHASITH VORANITIYAOWAPA <61766926+ohmspeed777@users.noreply.github.com>
2021-07-12 15:02:43 +0700

    rename variable and method

--- src/com/swa/oop/Product.java
+++ src/com/swa/oop/Product.java
@@ -4,12 +4,12 @@ public class Product {
     private String productID;
     private String productName;
 
-    Product(String p_id, String p_name) {
+    Product(String p_id, String pName) {
         this.productID = p_id;
-        this.productName = p_name;
+        this.productName = pName;
     }
 
-    public String getDetailProduct() {
+    public String getDetail() {
         return "com.swa.oop.Product ID : " + this.productID + " and com.swa.oop.Product Name : " + this.productName;
     }
 }
